e2f15ca55ef19adb6f79e694138056b0a927bd85
maccam6@gmail.com <maccam6@gmail.com>
2019-01-27 09:14:10 -0500

    update

--- Philips_hue_control/hue_control_interface.py
+++ Philips_hue_control/hue_control_interface.py
@@ -7,33 +7,76 @@ from tkinter import *
 from phue import Bridge, Light
 
 def set_values(value=None):
-    test_light.brightness = s1.get()
+    test_light.brightness = sa1.get()
 
 def set_temp(value=None):
-    test_light.colortemp_k = s2.get()
+    test_light.colortemp_k = sa2.get()
 
 def turnOn():
     test_light.on = True
 
 def turnOff():
     test_light.on = False
+
+def switch():
+    print('Light switch {}' .format(la1_var.get()))
+    test_light.on = la1_var.get()
     
+    set_scale_state()
+
+def set_scale_state():
+    state_test_light = test_light.on
+    if state_test_light:
+        sa1.config(state=NORMAL, fg='#000000')
+        sa2.config(state=NORMAL, fg='#000000')
+    else:
+        sa1.config(state=DISABLED, fg='#808080')
+        sa2.config(state=DISABLED, fg='#808080')
 
 b = Bridge('10.0.0.2')
 lights = b.lights
 test_light = Light(b, 5)
-test_light.colortemp_k = 3800
-test_light.on = True
+light_a = Light(b, 5)
+light_b = Light(b, 3)
+
+
+#test_light.on = True
+#test_light.colortemp_k = 3200
+
+#test_light.brightness = 50
+
+state_test_light_a = light_a.on
+default_brightness_a = light_a.brightness
+default_color_temp_a = light_a.colortemp_k
+
+state_test_light_b = light_b.on
+default_brightness_b = light_b.brightness
+default_color_temp_b = light_b.colortemp_k
 
 root=Tk()
+la1_var=BooleanVar(None, state_test_light_a)
+sa1_var = IntVar(None, default_brightness_a)
+sa2_var = IntVar(None, default_color_temp_a)
+sa1 = Scale(root, from_=0, to=254, orient=HORIZONTAL, variable=sa1_var, command=set_values)
+sa2 = Scale(root, from_=2000, to=6500, orient=HORIZONTAL, variable=sa2_var, command=set_temp)
 
-s1 = Scale(root, from_=0, to=254, orient=HORIZONTAL, command=set_values)
-s2 = Scale(root, from_=2000, to=6500, orient=HORIZONTAL, command=set_temp)
-#                                                ^^^^^^^^^^^^^^^^^^^
-#s1.set(19)
-s1.pack()
-s2.pack()
+lb1_var=BooleanVar(None, state_test_light_b)
+sb1_var = IntVar(None, default_brightness_b)
+sb2_var = IntVar(None, default_color_temp_b)
+sb1 = Scale(root, from_=0, to=254, orient=HORIZONTAL, variable=sb1_var, command=set_values)
+sb2 = Scale(root, from_=2000, to=6500, orient=HORIZONTAL, variable=sb2_var, command=set_temp)
 
-Button(root, text='On', command=turnOn).pack()
-Button(root, text='Off', command=turnOff).pack()
+
+sa1.pack()
+sa2.pack()
+sb1.pack()
+sb2.pack()
+
+ra1=Radiobutton(text='On', variable=la1_var, value=True ,width=6, indicatoron=False, command=switch).pack(side='left')
+ra2=Radiobutton(text='Off', variable=la1_var, value=False ,width=6, indicatoron=False, command=switch).pack(side='left')
+rb1=Radiobutton(text='On', variable=lb1_var, value=True ,width=6, indicatoron=False, command=switch).pack(side='left')
+rb2=Radiobutton(text='Off', variable=lb1_var, value=False ,width=6, indicatoron=False, command=switch).pack(side='left')
+
+set_scale_state()
+
 root.mainloop()
--- Philips_hue_control/test.py
+++ Philips_hue_control/test.py
@@ -6,7 +6,7 @@ Created on Dec. 29, 2018
 '''
 
 
-from phue import Bridge, Light
+from phue import Bridge, Light, AllLights, Group
 from time import sleep, time
 
 def set_values(value=None):
@@ -59,5 +59,64 @@ b = Bridge('10.0.0.2')
 lights = b.lights
 test_light = Light(b, 5)
 color = [1, 6000, 12000, 18000, 24000, 30000, 36000, 42000, 48000, 52000]
+
+# test_light.on = False
+# sleep(2)
+# test_light.on = True
+# sleep(2)
+# test_light.on = False
+# sleep(2)
+print(b.groups)
+
+nb_group = b.groups
+#print(len(nb_group))
+
+print(b.get_group_id_by_name('control_group'))
+
+#print(b.lights)
+
+#b.create_group('control_group', [3, 4, 5])
+b.set_group(group_id='control_group', parameter='lights', value=[5])
+
+gr0 = Group(b, 'control_group')
+gr0.on = True
+gr0.brightness = 254
+gr0.colortemp_k = 3500
+sleep(3)
+gr0.on = False
+b.set_group(group_id='control_group', parameter='lights', value=[3, 4])
+
+gr0 = Group(b, 'control_group')
+gr0.on = True
+gr0.brightness = 254
+gr0.colortemp_k = 3500
+sleep(3)
+gr0.on = False
+# print(b.get_group_id_by_name('control_group'))
+
+
+
+print(b.groups)
+
+# b.create_group('control_group', [5])
+# gr0 = Group(b, 'control_group')
+# sleep(1)
+# gr0.on = True
+# gr0.brightness = 254
+# gr0.colortemp_k = 3500
+# sleep(3)
+# gr0.on = False
+
 #colorChange(0, 3, color)
-flash(0.1, 0.1, 100)
+#flash(0.1, 0.1, 100)
+# test_light.on = True
+# test_light.colortemp_k = 3300
+# test_light.brightness = 50
+# test_light.transitiontime = 2
+# test_light.on = True
+# sleep(3)
+# test_light.brightness = 100
+# sleep(1)
+# test_light.brightness = 0
+# sleep(3)
+# test_light.on = False
