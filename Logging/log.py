2b3a76ea12e5a864b57534741fd8d1cb89b3cbb0
maccam6@gmail.com <maccam6@gmail.com>
2019-02-10 07:49:17 -0500

    finish version

--- Logging/log.py
+++ Logging/log.py
@@ -3,9 +3,8 @@
 '''
 Created on 8 f�vr. 2019
 
-@author: marti
+@author: martin
 '''
-import os
 import datetime
 from time import sleep
 # from urllib3.util.timeout import current_time
@@ -23,25 +22,26 @@ class mylog(object):
         '''
         self.fileName = fileName
         self.dateStamp = dateStamp
-        currentTime = datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")
-        self.fullFileName = currentTime + '_' + fileName + '.txt'
-        with open(currentTime + '_' + fileName + '.txt', 'a') as fh:
-            fh.write("File create\n")
-        print(currentTime)
+        if dateStamp:
+            currentTime = datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")
+            self.fullFileName = currentTime + '_' + fileName + '.txt'
+            
+        else:
+            self.fullFileName = fileName + '.txt'
+        with open(self.fullFileName, 'w'):
+            pass
     
     def addLog(self, message):
         with open(self.fullFileName, 'a') as fh:
             currentTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
             fh.write(currentTime + ': ' + message + '\n')
-        print("add")
         
-l = mylog('Martin')
+l = mylog('Martin', False)
 l.addLog('AllO')
 
 for i in range(10):
-#     currentTime = datetime.datetime.now()
     l.addLog('Ecrit la ligne' + str(i))
-    assert i != 5, 'Mauvaise valeur'
+    #assert i != 5, 'Mauvaise valeur'
     sleep(0.1)
     
     
